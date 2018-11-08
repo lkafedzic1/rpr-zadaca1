e3af2a59d39b08e308a73839e0716da956a525bf
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-08 09:45:04 +0100

    Dodane prve 2 metode

---
+++ src/ba/unsa/etf/rpr/Chesspiece.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+public class Chesspiece {
+    private String pozicija;
+    private String boja;
+    public enum Color{BLACK,WHITE};
+    public String getPosition() {return null;}
+    public String getColor() {return null;}
+
+
+}
