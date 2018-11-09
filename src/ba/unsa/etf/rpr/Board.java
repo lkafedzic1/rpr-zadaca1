6a8b569cbfd3fb8ffc00ddd1fc9ee71a9ab0a613
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-09 11:03:36 +0100

    Interface

---
+++ src/ba/unsa/etf/rpr/Board.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr;
+
+public class Board {
+
+    public Board() {}
+    public void move(Class type, ChessPiece.Color color, String position) {}
+    public void move(String oldPosition, String newPosition){}
+    public boolean isCheck(ChessPiece.Color color) {return false;}
+}
