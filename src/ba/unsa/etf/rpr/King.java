689b0a202aedeae8b59f6bf3e5f3c8821b1fadb6
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-09 14:43:12 +0100

    Prepravka string sa color

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr;
 
-public class King {
+public class King extends ChessPiece{
+    public King(String position, ChessPiece.Color color) {
+        super(position,color);
+
+    }
+    @Override
+    public void move(String n_position) throws IllegalChessMoveException {
+        //kretanje u desnu stranu i lijevu stranu, gore,dolje, dijagonalno
+        if((this.position.charAt(0)+1==n_position.charAt(0) && this.position.charAt(1)==n_position.charAt(1))
+        || (this.position.charAt(0)-1==n_position.charAt(0) && this.position.charAt(1)==n_position.charAt(1))
+            || (this.position.charAt(0)==n_position.charAt(0) && this.position.charAt(1)+1==n_position.charAt(1))
+        || (this.position.charAt(0)==n_position.charAt(0) && this.position.charAt(1)-1==n_position.charAt(1))
+
+        || (this.position.charAt(0)+1==n_position.charAt(0) && this.position.charAt(1)+1==n_position.charAt(1))
+        || (this.position.charAt(0)-1==n_position.charAt(0) && this.position.charAt(1)-1==n_position.charAt(1))
+        || (this.position.charAt(0)+1==n_position.charAt(0) && this.position.charAt(1)-1==n_position.charAt(1))
+        || (this.position.charAt(0)-1==n_position.charAt(0) && this.position.charAt(1)+1==n_position.charAt(1)))
+            position=n_position;
+        else throw new IllegalChessMoveException();
+    }
 }
