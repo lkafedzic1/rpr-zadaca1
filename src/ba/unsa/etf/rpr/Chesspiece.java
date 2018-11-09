3df47e1af4b09f809a16f07298c532d287270d8f
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-09 11:02:34 +0100

    Interface

--- src/ba/unsa/etf/rpr/Chesspiece.java
+++ src/ba/unsa/etf/rpr/Chesspiece.java
@@ -1,10 +1,11 @@
 package ba.unsa.etf.rpr;
 
-public class Chesspiece {
+public class ChessPiece {
+
     private String pozicija;
     private String boja;
     public enum Color{BLACK,WHITE};
-    public Chesspiece(String pozicija, String boja) {}
+    public ChessPiece(String pozicija, String boja) {}
     public String getPosition() {return null;}
     public String getColor() {return null;}
     public void move(String position) {}
