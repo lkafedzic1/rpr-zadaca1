02e3c9ac1f81f04543a1d7ca00d1f23fd0f18f0a
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-09 21:40:12 +0100

    commit

--- src/ba/unsa/etf/rpr/Rook.java
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -1,9 +1,19 @@
 package ba.unsa.etf.rpr;
 
+import static java.lang.Math.abs;
+
 public class Rook extends ChessPiece {
 
-    public Rook(String pozicija, String boja) {
-        super(pozicija, boja);
+    public Rook(String position, Color color) {
+        super(position, color);
     }
+    @Override
+    public void move(String n_position) throws IllegalArgumentException, IllegalChessMoveException {
+        if (!isLegalPosition(n_position)) throw new IllegalArgumentException();
+        else if ((abs(position.charAt(0)-n_position.charAt(0))<=1 && abs(position.charAt(1)-n_position.charAt(1))==0) ||
+                (abs(position.charAt(0)-n_position.charAt(0))==0 && abs(position.charAt(1)-n_position.charAt(1))<=1))
+            position = n_position;
+        throw new IllegalChessMoveException();
 
+    }
 }
