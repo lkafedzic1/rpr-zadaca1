c4cca9660f361d4cb384591cd486b7bd860dfd2e
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-10 17:39:07 +0100

    Dodane nove provjere u testovima

--- test/ba/unsa/etf/rpr/BishopTest.java
+++ test/ba/unsa/etf/rpr/BishopTest.java
@@ -6,13 +6,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BishopTest {
 
-
-
     @Test
-    void move() {
+    void moveDiagonal() {
         Bishop b = new Bishop("D5", ChessPiece.Color.WHITE);
         assertDoesNotThrow(
                 () -> b.move("F7")
         );
     }
+    @Test
+    void moveBack() {
+        Bishop b = new Bishop("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> b.move("D2")
+        );
+    }
+    @Test
+    void moveLeftt() {
+        Bishop b = new Bishop("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> b.move("C3")
+        );
+    }
+    @Test
+    void moveRight() {
+        Bishop b = new Bishop("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> b.move("E3")
+        );
+    }
+    @Test
+    void moveFront() {
+        Bishop b = new Bishop("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> b.move("D7")
+        );
+    }
+    @Test
+    void constructor1() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Bishop("I2", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @Test
+    void constructor2() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Bishop("B9", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @Test
+    void constructor3() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Bishop("", ChessPiece.Color.WHITE)
+        );
+    }
+
 }
--- test/ba/unsa/etf/rpr/KnightTest.java
+++ test/ba/unsa/etf/rpr/KnightTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KnightTest {
 
+
+
     @Test
     void move() {
         Knight k = new Knight("B1", ChessPiece.Color.WHITE);
@@ -13,5 +15,68 @@ class KnightTest {
                 () -> k.move("C3")
         );
 
+    }
+    @Test
+    void moveDiagonal() {
+        Knight k = new Knight("D5", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> k.move("F7")
+        );
+    }
+    @Test
+    void moveBack() {
+        Knight n = new Knight("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> n.move("D2")
+        );
+    }
+    @Test
+    void moveLeftt() {
+        Knight n = new Knight("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> n.move("C3")
+        );
+    }
+    @Test
+    void moveRight() {
+        Knight n = new Knight ("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> n.move("E3")
+        );
+    }
+    @Test
+    void moveFront() {
+        Knight n = new Knight("D3", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> n.move("D7")
+        );
+    }
+    @Test
+    void constructor1() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Knight("I2", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @Test
+    void constructor2() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Knight("B9", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @Test
+    void constructor3() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Knight("", ChessPiece.Color.WHITE)
+        );
+    }
+    @Test
+    void constructor4() {
+        assertThrows(IllegalArgumentException.class,
+                ()->new Knight("Start",ChessPiece.Color.WHITE));
     }
 }
