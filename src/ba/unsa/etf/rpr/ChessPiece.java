69305e978e2e88033c28794800903f42a9bff916
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-10 10:30:51 +0100

    commit

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -1,19 +1,19 @@
 package ba.unsa.etf.rpr;
 
 public class ChessPiece {
-    private boolean isLegalPosition(String s) {
-        if(s.length()!=2 || s.charAt(0)<'A' || s.charAt(0)>'H' || s.charAt(1)<'0' || s.charAt(1)>'8' ) return false;
-        else return true;
+    boolean isLegalPosition(String s) {
+        if(s.length()==2 && ((s.charAt(0)>='A' && s.charAt(0)<='H') || (s.charAt(0)>='a' && s.charAt(0)<='h'))
+                && (s.charAt(1)>'0' && s.charAt(1)<='8') ) return true;
+        return false;
     }
 
-    private String position;
-    private String color;
+    protected String position;
+    protected Color color;
     public enum Color{BLACK,WHITE};
-    public ChessPiece(String pozicija, String boja) {}
-    public String getPosition() {return this.position;}
-    public String getColor() {return this.color;}
-    public void move(String position) {
-
+    public ChessPiece(String position, Color color) throws IllegalArgumentException {
+        if (!isLegalPosition(position)) throw new IllegalArgumentException();
     }
-
+    public String getPosition() {return this.position;}
+    public Color getColor() {return this.color;}
+    public void move(String position) throws IllegalChessMoveException, IllegalArgumentException {}
 }
